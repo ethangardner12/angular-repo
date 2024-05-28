fef202a85764ad0adce6877ee8b27c7f7969bc84
Ethan Gardner <ethan.gardner@find-cifas.org.uk>
2024-05-28 11:12:06 +0100

    shopping app and all components on one page. Minimal logic.

--- shopping-app/src/app/recipesComponent/recipeList/recipe-list.component.ts
+++ shopping-app/src/app/recipesComponent/recipeList/recipe-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Test description for the recipe', 'https://th.bing.com/th/id/OIP.2NBaWNO2F9V5wXwA8ftOuwHaE7?w=240&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'),
+    new Recipe('Steak Recipe', 'Description for the steak recipe', 'https://th.bing.com/th/id/OIP.Y9JT709sakFUcfUuKkTMJwHaHa?w=186&h=186&c=7&r=0&o=5&pid=1.7')
+  ];
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  
+  constructor(){
 
+  }
 }
