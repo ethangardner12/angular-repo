c9f82e330b78825241ba9481e8b31a9877b43a6b
Ethan Gardner <ethan.gardner@find-cifas.org.uk>
2024-05-30 17:25:26 +0100

    Custom property and event binding

--- servers-databinding/src/app/cockpit/cockpit.component.ts
+++ servers-databinding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,24 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class CockpitComponent implements OnInit {
   newServerName = '';
   newServerContent = '';
+
+  @Output('srvCreated') serverCreated = new EventEmitter<{
+    serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{
+    blueprintName: string, blueprintContent: string}>();
   
   constructor() {}
 
   ngOnInit(){}
 
   onAddServer() {
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
   }
 
   onAddBlueprint() {
-    // this.serverElements.push({
-    //   type: 'blueprint',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.blueprintCreated.emit({
+      blueprintName: this.newServerName,
+      blueprintContent: this.newServerContent
+    });
   }
 }
