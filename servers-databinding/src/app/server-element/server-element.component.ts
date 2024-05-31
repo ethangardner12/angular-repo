4306b18639a2279f7db32789e6b0beff2c64957d
Ethan Gardner <ethan.gardner@find-cifas.org.uk>
2024-05-31 12:02:50 +0100

    access to ng-content w/ @ContentChild( )

--- servers-databinding/src/app/server-element/server-element.component.ts
+++ servers-databinding/src/app/server-element/server-element.component.ts
@@ -3,6 +3,7 @@ import { AfterContentChecked,
   AfterViewChecked,
   AfterViewInit,
   Component,
+  ContentChild,
   DoCheck,
   ElementRef,
   Input,
@@ -25,6 +26,7 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
   @Input('srvElement') element: {type: string, name: string, content: string};
   @Input() name: string;
   @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
 
   constructor() {
     console.log('constructor called');
@@ -38,6 +40,7 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
   ngOnInit() {
     console.log('ngOnInit called');
     console.log('text content onInit ' + this.header.nativeElement.textContent);
+    console.log('Text content of paragraph ' + this.paragraph.nativeElement.textContent);
   }
 
   ngDoCheck(){
@@ -46,6 +49,7 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
 
   ngAfterContentInit(){
     console.log('ngAfterContentInit called');
+    console.log('Text content of paragraph (afterContentInit) ' + this.paragraph.nativeElement.textContent);
   }
 
   ngAfterContentChecked(){
