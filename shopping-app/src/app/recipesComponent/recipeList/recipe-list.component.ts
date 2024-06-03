79cdef3dffc57c610cd4746d0378dfb495cfb3f5
Ethan Gardner <ethan.gardner@find-cifas.org.uk>
2024-06-03 10:08:31 +0100

    passing data with event and property binding combined

--- shopping-app/src/app/recipesComponent/recipeList/recipe-list.component.ts
+++ shopping-app/src/app/recipesComponent/recipeList/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';//imports recipe to utilise array of recipes
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';//imports recipe to utilise array of re
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('Test Recipe', 'Test description for the recipe', 'https://th.bing.com/th/id/OIP.2NBaWNO2F9V5wXwA8ftOuwHaE7?w=240&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'),
     new Recipe('Steak Recipe', 'Description for the steak recipe', 'https://th.bing.com/th/id/OIP.Y9JT709sakFUcfUuKkTMJwHaHa?w=186&h=186&c=7&r=0&o=5&pid=1.7')
@@ -16,4 +18,8 @@ export class RecipeListComponent implements OnInit {
   }
   
   constructor(){}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
